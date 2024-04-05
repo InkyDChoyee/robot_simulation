5b4ac210b3ece6bcfe47a1c3134ef7ac18d02c83
InkyDChoyee <choyee0421@naver.com>
2024-04-05 14:31:52 +0900

    Feat: Building component

--- src/components/Building.js
+++ src/components/Building.js
@@ -1,5 +1,18 @@
-const Building = () => {
-  return <div></div>;
+import React from "react";
+
+const Building = ({ children }) => {
+  return (
+    <div
+      style={{
+        position: "relative",
+        width: "500px",
+        height: "500px",
+        border: "1px solid black",
+      }}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Building;
