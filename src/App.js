82cee016584bc8a331070d68bc1e1d1b756acd77
InkyDChoyee <choyee0421@naver.com>
2024-04-05 14:06:32 +0900

    Feat: set position

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import "./App.css";
+import React, { useState } from "react";
+import Robot from "./components/Robot";
+import Building from "./components/Building";
+import { findPath } from "./utils/pathfinding";
 
 function App() {
+  const [robotPosition, setRobotPosition] = useState({ x: 0, y: 0 });
+  const [devices, setDevices] = useState([
+    { type: "bell", position: { x: 2, y: 3 } },
+    { type: "elevator", position: { x: 5, y: 5 } },
+    { type: "chime", position: { x: 8, y: 2 } },
+  ]);
+
   return <div className="App"></div>;
 }
 
